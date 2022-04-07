6c7178dffca5dddfc18a223f66ff12c5964d1828
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-07 10:36:17 -0700

    add shelljs command to build yaml component

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+const shell = require('shelljs');
+
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('"gdsfactory" extension is now active!');
@@ -6,18 +8,31 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+
+	let yaml_run = vscode.commands.registerCommand('gdsfactory.yaml_run', async () => {
+        let command = "gf yaml build /home/jmatres/ubc/ubcpdk/circuits/mask.ic.yaml";
+        shell.exec(command, { async: true }, async function (code:number, stdout:string, stderr:string) {
+            console.log("Exec gdsfactory");
+			 if (code === 0) {
+				vscode.window.showInformationMessage('Error!');
+
+            }
+            else {
+				vscode.window.showInformationMessage('works');
+
+            }
+        });
+    });
+
+	context.subscriptions.push(yaml_run);
+
 	let disposable = vscode.commands.registerCommand('gdsfactory.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from gdsfactory!!!!!!!!!!');
+		vscode.window.showInformationMessage('Hello World from gdsfactory!!!!!');
 	});
+
 	context.subscriptions.push(disposable);
-
-	let hi = vscode.commands.registerCommand('gdsfactory.hi', () => {
-		vscode.window.showInformationMessage('Hi from gdsfactory!');
-	});
-
-	context.subscriptions.push(hi);
 }
 
 // this method is called when your extension is deactivated
