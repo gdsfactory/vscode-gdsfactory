49761b64b17b8ac83229b93149514ef666f120eb
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-05-27 07:21:21 -0700

    works with vscode 1.50 and larger

--- src/extension.ts
+++ src/extension.ts
@@ -32,9 +32,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(yamlRun);
 
-	let disposable = vscode.commands.registerCommand('gdsfactory.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	let disposable = vscode.commands.registerCommand('gdsfactory.yamlWatch', () => {
 		vscode.window.showInformationMessage('Hello World from gdsfactory!!!!!');
 	});
 
