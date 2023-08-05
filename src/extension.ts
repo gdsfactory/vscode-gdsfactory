3a2f9bd152ff287fe167a589c38dc052a60a7501
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-08-05 15:43:25 -0700

    add filewatcher button

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,11 @@
 import * as vscode from 'vscode';
-const shell = require('shelljs');
-
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('"gdsfactory" extension is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-
-	let yamlRun = vscode.commands.registerCommand('gdsfactory.yamlRun', async () => {
-		if (!vscode.window.activeTextEditor) {
-					return; // no editor
-				}
-		let document = vscode.window.activeTextEditor.document;
-        let filepath = document.fileName;
-        let command = "gf yaml watch " + filepath;
-		console.log(command);
-        shell.exec(command, { async: true }, async function (code:number, stdout:string, stderr:string) {
-            console.log("Exec gdsfactory");
-			 if (code === 0) {
-				vscode.window.showInformationMessage('watch folder and show changes in klayout!');
-
-            }
-            else {
-				vscode.window.showInformationMessage('works');
-
-            }
-        });
+    let disposable = vscode.commands.registerCommand('gdsfactory.watch', () => {
+        let terminal = vscode.window.createTerminal("gdsfactory");
+        terminal.show();
+        terminal.sendText('gf watch $PWD');
     });
 
-	context.subscriptions.push(yamlRun);
-
-	let disposable = vscode.commands.registerCommand('gdsfactory.yamlWatch', () => {
-		vscode.window.showInformationMessage('Hello World from gdsfactory!!!!!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
